e516f8cf8471d18c442ee9bd3bb4312071017802
Gayan <gayan@track-back.co>
2021-10-22 10:32:23 +1300

    Deployment configuration

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -6,12 +5,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var ipfsRouter = require('./routes/ipfs');
-
+const cors = require('cors');
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
