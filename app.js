8d8705d444659627dd64f028bbb9ab00516bdc24
Don <don@track-back.co>
2021-11-03 09:34:05 +1300

    remove terraform from project

--- app.js
+++ app.js
@@ -18,14 +18,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/0.1/', indexRouter);
 app.use('/api/0.1/ipfs/', ipfsRouter);
 
+app.get('/health', (req, res) => { return res.sendStatus(200); })
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.statusCode = 400;
-  res.json({error: "Invalid Request"})
+  res.json({ error: "Invalid Request" })
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
